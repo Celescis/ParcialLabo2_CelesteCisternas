27c4181a8860c97ce882c5fbe18497b31272333a
Celeste Cisternas <celestecisternas@gmail.com>
2021-06-24 09:51:23 -0300

    Initial commit

---
+++ src/Segundo Parcial.c
@@ -0,0 +1,16 @@
+/*
+ ============================================================================
+                     SEGUNDO PARCIAL LABORATORIO 2021
+ Nombre      : Celeste Cisternas
+ Curso       : 1ro B
+
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
